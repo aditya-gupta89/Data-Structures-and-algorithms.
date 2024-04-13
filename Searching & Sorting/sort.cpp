625d378108de2519bd0b48f0c1c0c3a8ee94767f
Aditya <adityagupta8023@gmail.com>
2024-04-13 21:05:09 +0530

    feat : add the bubble sort and insertion sort

--- Searching & Sorting/sort.cpp
+++ Searching & Sorting/sort.cpp
@@ -18,9 +18,49 @@ void selectionSort(int arr[], int n)
         cout << arr[i] << " ";
 }
 
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int tem = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tem;
+            }
+            else
+                break;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int tem = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tem;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
+
 int main()
 {
     int arr[] = {4, 3, 44, 23, 12, 64, 2};
-    selectionSort(arr, sizeof(arr) / sizeof(arr[0]));
+    bubbleSort(arr, sizeof(arr) / sizeof(arr[0]));
     return 0;
 }
